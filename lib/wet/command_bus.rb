d15990d67bd70d20586d7acd713590f9ef592d9e
Paweł Pacana <pawel.pacana@gmail.com>
2016-05-30 17:09:20 +0200

    Protect from defining multiple handler per command.
    * easy to make this error
    * it is one handler per command by design

--- lib/wet/command_bus.rb
+++ lib/wet/command_bus.rb
@@ -4,6 +4,7 @@ require 'thread_safe'
 module Wet
   class CommandBus
     UnregisteredHandler = Class.new(StandardError)
+    MultipleHandlers    = Class.new(StandardError)
 
     def initialize
       @handlers =
@@ -11,20 +12,17 @@ module Wet
     end
 
     def register(klass, handler)
+      raise MultipleHandlers.new("Multiple handlers not allowed for #{klass}") if handlers[klass]
       handlers[klass] = handler
     end
 
     def call(command)
       handlers
-        .fetch(command.class) { raise UnregisteredHandler.new(message(command))  }
+        .fetch(command.class) { raise UnregisteredHandler.new("Missing handler for #{command.class}")  }
         .(command)
     end
 
     private
     attr_reader :handlers
-
-    def message(command)
-      "Missing handler for #{command.class}"
-    end
   end
 end
--- spec/command_bus_spec.rb
+++ spec/command_bus_spec.rb
@@ -23,5 +23,14 @@ module Wet
           expect(err.message)
             .to(eq('Missing handler for FooCommand')) })
     end
+
+    specify 'one command, one handler' do
+      bus = CommandBus.new
+      bus.register(::FooCommand, ->{})
+      expect{bus.register(::FooCommand, ->{})}
+        .to(raise_error(CommandBus::MultipleHandlers) { |err|
+          expect(err.message)
+            .to(eq('Multiple handlers not allowed for FooCommand')) })
+    end
   end
 end
